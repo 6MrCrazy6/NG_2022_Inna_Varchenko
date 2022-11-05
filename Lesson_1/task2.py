b6589f2d65ece3d06c20be798d0e899ae21b79cd
6MrCrazy6 <117535858+6MrCrazy6@users.noreply.github.com>
2022-11-05 20:30:48 +0200

    Update task2.py

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,1 +1,28 @@
-
+import math
+exam = input('Що мені робити з цими числами? (Введіть "+", "-", "*", "/", "^", "k") ')
+if exam == '^' or exam == 'k':
+    x = int(input('Введіть число: '))
+    if exam == '^':
+        z = math.pow(x,2)
+        print('Квадрат числа дорівнює: ', z)
+    elif exam == 'k':
+        z = math.sqrt(x)
+        print('Корінь числа дорівнює: ', z)
+else:
+    x = int(input('Введіть перше число: '))
+    y = int(input('Введіть друге число: '))
+    if exam == '+':
+        z = x+y
+        print('Результат додавання: ', z)
+    elif exam == '-':
+        z = x-y
+        print('Результат віднімання: ', z)
+    elif exam == '*':
+        z = x*y
+        print('Результат множення: ', z)
+    elif exam == '/':
+        if y == 0:
+            print('На 0 делить нельзя!')
+        else:
+            z = x/y
+            print('Результат ділення: ', z)
